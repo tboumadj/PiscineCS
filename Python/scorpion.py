54cf5588e5328de8ed033a329008a34eab51446b
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-05-17 17:17:04 +0200

    Add files via upload

---
+++ Python/scorpion.py
@@ -0,0 +1,21 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    scorpion.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/05/17 15:59:18 by tboumadj          #+#    #+#              #
+#    Updated: 2023/05/17 16:57:58 by tboumadj         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+if __name == "__main__":
+# parsing param
+    parser = argparse.ArgumentParser(prog='scoprion')
+    parser.add_argument('File', help='File to extract')
+    args = parser.parse_args()
+#Test------------------
+
+#--------------------------------------------------
+
