023f00968e141235134df46d1cbdfb05b44018b1
tboumadj <93995176+tboumadj@users.noreply.github.com>
2023-05-18 16:55:29 +0200

    Add files via upload

--- Python/scorpion.py
+++ Python/scorpion.py
@@ -6,16 +6,38 @@
 #    By: tboumadj <tboumadj@student.42mulhouse.fr>  +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/17 15:59:18 by tboumadj          #+#    #+#              #
-#    Updated: 2023/05/17 16:57:58 by tboumadj         ###   ########.fr        #
+#    Updated: 2023/05/18 16:16:45 by tboumadj         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-if __name == "__main__":
+from PIL import Image
+from PIL.ExifTags import TAGS
+import argparse
+
+def extract_data(path):
+    try:
+        img = Image.open(path)
+    except Exception as e:
+        print("cannot open file...")
+        return
+    exifdata = img.getexif()
+    exif = {}
+    for tag_id, value in exifdata.items():
+        tag = TAGS.get(tag_id, tag_id)
+        data = exifdata.get(tag_id)
+        if exifdata is not None:
+            for tag in exifdata:
+                print(f"{tag}: {data}")
+        else:
+            print("none data exif")
+
+if __name__ == "__main__":
 # parsing param
     parser = argparse.ArgumentParser(prog='scoprion')
     parser.add_argument('File', help='File to extract')
     args = parser.parse_args()
 #Test------------------
-
+    print(args.File)
+    extract_data(args.File)
 #--------------------------------------------------
 
